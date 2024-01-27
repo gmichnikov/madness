1deaec7af91012b0e4585020a8b110a48bc09ca4
Greg Michnikov <greg.michnikov@gmail.com>
2024-01-27 11:34:27 -0500

    Allow users with the same email to register in different pools

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Pool(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     full_name = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     time_zone = db.Column(db.String(50), nullable=False, default='UTC')
